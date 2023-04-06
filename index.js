1f8130fcd8b7fe9c1ecea79a1dffd1e338ac50b5
yosiefberhe <70027040+yosiefberhe@users.noreply.github.com>
2023-04-06 10:10:25 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === "user" && password === "password") {
+      // Navigate to dashboard or home page on successful login
+      console.log("Login successful");
+    } else {
+      setError("Invalid username or password");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      {error && <p className="error">{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
